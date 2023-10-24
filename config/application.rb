815ecd15634f3e32e2f2e928f9b5b1dccc0809e9
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-10-24 15:30:33 +0500

    complete_project

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,13 @@ module HelloRailsBackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # You can specify specific origins here, e.g., 'example.com'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
